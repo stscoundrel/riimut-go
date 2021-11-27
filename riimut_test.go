9e706348314b26da909a78306f5cc85eeaa5030c
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-11-27 21:35:16 +0200

    Module tests: add elder futhark

--- riimut_test.go
+++ riimut_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"testing"
 
+	"github.com/stscoundrel/riimut/elderfuthark"
 	"github.com/stscoundrel/riimut/youngerfuthark"
 )
 
@@ -22,6 +23,28 @@ func TestToungerFutharkransformsRunesToletters(t *testing.T) {
 	const content = "ᛅᚢᚴ:ᛏᛅᚾᛁ:ᚴᛅᚱᚦᛁ:ᚴᚱᛁᛋᛏᚾᛅ"
 	const expected = "auk tani karþi kristna"
 	result := youngerfuthark.RunesToLetters(content)
+
+	if result != expected {
+		t.Error("Did not transform runes to letters. Received", result, "expected ", expected)
+	}
+}
+
+func TestElderFutharkTransformsLettersToRunes(t *testing.T) {
+	// From 4th century axe in Jutland
+	const content = "wagagastiz alu wihgu sikijaz aiþalataz"
+	const expected = "ᚹᚨᚷᚨᚷᚨᛋᛏᛁᛉ:ᚨᛚᚢ:ᚹᛁᚻᚷᚢ:ᛋᛁᚲᛁᛃᚨᛉ:ᚨᛁᚦᚨᛚᚨᛏᚨᛉ"
+	result := elderfuthark.LettersToRunes(content)
+
+	if result != expected {
+		t.Error("Did not transform letters to runes. Received", result, "expected ", expected)
+	}
+}
+
+func TestElderFutharkransformsRunesToletters(t *testing.T) {
+	// From 4th century axe in Jutland
+	const content = "ᚹᚨᚷᚨᚷᚨᛋᛏᛁᛉ:ᚨᛚᚢ:ᚹᛁᚻᚷᚢ:ᛋᛁᚲᛁᛃᚨᛉ:ᚨᛁᚦᚨᛚᚨᛏᚨᛉ"
+	const expected = "wagagastiz alu wihgu sikijaz aiþalataz"
+	result := elderfuthark.RunesToLetters(content)
 
 	if result != expected {
 		t.Error("Did not transform runes to letters. Received", result, "expected ", expected)
