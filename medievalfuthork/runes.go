a9b854b39a3832cb45140c325c633447002fd738
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-11-27 21:21:16 +0200

    Add Medieval Futhork package

---
+++ medievalfuthork/runes.go
@@ -0,0 +1,33 @@
+package medievalfuthork
+
+func getRuneMapping() map[string]string {
+	return map[string]string{
+		"ᚠ": "f",
+		"ᚢ": "u",
+		"ᚦ": "þ",
+		"ᚮ": "o",
+		"ᚱ": "r",
+		"ᚴ": "k",
+		"ᚼ": "h",
+		"ᚿ": "n",
+		"ᛁ": "i",
+		"ᛆ": "a",
+		"ᛌ": "s",
+		"ᛋ": "s",
+		"ᛐ": "t",
+		"ᛏ": "t",
+		"ᛒ": "b",
+		"ᛘ": "m",
+		"ᛚ": "l",
+		"ᛦ": "y",
+		":": " ",
+
+		// Sting diacritic secondary sounds.
+		"ᚯ": "ø",
+		"ᛅ": "æ",
+		"ᚰ": "ǫ",
+		"ᛕ": "ᴘ",
+		"ᚽ": "e",
+		"ᚵ": "g",
+	}
+}
